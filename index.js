a927eb1637b5388bf84fd66abfd4981b9ccfbee5
Yahya Mohamed <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-07-04 21:03:25 +0300

    Made the to-do app fully functional.

---
+++ index.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const myForm = document.querySelector("form");
+  const myInput = document.querySelector("input");
+  const myDiv = document.querySelector(".container");
+  const myUl = document.createElement("ul");
+
+  // Add submit event to form
+  myForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const myInputValue = myInput.value;
+
+    // Create a new <li> element for each task
+    const myLi = document.createElement("li");
+    myLi.textContent = myInputValue;
+    myLi.style.textAlign = "left";
+    myLi.style.marginTop = "10px";
+
+    // Add click event to li so as to click to remove it
+    myLi.addEventListener("click", function (e) {
+      e.target.remove();
+    });
+
+    myUl.appendChild(myLi);
+    myDiv.appendChild(myUl);
+    document.body.append(myDiv);
+    console.log(myInputValue);
+
+    // Clear the input field after adding the task
+    myInput.value = "";
+  });
+});
